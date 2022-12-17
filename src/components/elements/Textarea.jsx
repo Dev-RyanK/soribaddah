2c8da15a74800cc9963974fce03b3670364c98f9
Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-12-17 14:25:57 +0900

    feat: defaultProps 추가

--- src/components/elements/Textarea.jsx
+++ src/components/elements/Textarea.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import styled from "styled-components"
 
-const Textarea = ({ children, hidden }) => {
-  return <StTextarea hidden={hidden}>{children}</StTextarea>
+const Textarea = ({ hidden }) => {
+  return <textarea hidden={hidden} />
 }
 
 export default Textarea
 
+// 스타일을 적용하면 hidden이 안 먹힌다
 const StTextarea = styled.textarea`
   display: flex;
   border: 2px solid var(--color-midblue);
