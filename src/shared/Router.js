0c4b0c2f114a730767ab1debd5b15c11a12f7787
Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-12-17 21:18:46 +0900

    feat: Header, Layout 연결

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,23 +1,24 @@
 import React from "react"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Layout from "../shared/Layout"
+import Layout from "../components/Layout"
+import Header from "../components/Header"
 import Home from "../pages/Home"
 import PostDetail from "../pages/PostDetail"
 
 const Router = () => {
   return (
     <BrowserRouter>
-      {/* <Layout> */}
-      {/* 헤더 */}
-      <Routes>
-        <Route>
-          <Route path="/" element={<Home />} />
-          {/* 로그인 */}
-          {/* 회원가입 */}
-          <Route path="music/:id" element={<PostDetail />} />
-        </Route>
-      </Routes>
-      {/* </Layout> */}
+      <Layout>
+        <Header />
+        <Routes>
+          <Route>
+            <Route path="/" element={<Home />} />
+            {/* 로그인 */}
+            {/* 회원가입 */}
+            <Route path="music/:id" element={<PostDetail />} />
+          </Route>
+        </Routes>
+      </Layout>
     </BrowserRouter>
   )
 }
