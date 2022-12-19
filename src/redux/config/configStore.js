34068f25f96d3bde5c5410ece0c0d0f945ccb30b
Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-12-19 17:21:38 +0900

    feat: 회원가입, 상세페이지 토글 리듀서 추가

--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import post from "../modules/postDetailSlice"
+import signup from "../modules/signupSlice"
+import toggle from "../modules/togglePageSlice"
 
 const store = configureStore({
-  reducer: { post: post },
+  reducer: { post: post, signup: signup, toggle: toggle },
 })
 
 export default store
