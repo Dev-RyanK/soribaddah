50f3b273593407b123032c6b937f996e442d6859
newrkem <newrkem@gmail.com>
2022-12-20 17:51:45 +0900

    feat: 헤더css작업

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,10 @@ import classes from "./Header.module.css";
 
 const Header = () => {
   return (
-    <header className={classes.header}>
+    <header className={classes.head}>
       &nbsp;
       <h1>소리받아 </h1>
-      <button className={classes.h1}>로그인/회원가입</button>
+      <button className={classes.btn}>로그인/회원가입</button>
     </header>
   );
 };
