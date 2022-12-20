b4e440f14f130a49ba3549b0fe82443d17396a6a
Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-12-21 06:29:58 +0900

    feat: 카드 map 아래 요소 key 추가

--- src/pages/main/home/HomeCardList.jsx
+++ src/pages/main/home/HomeCardList.jsx
@@ -39,31 +39,44 @@ const HomeCardList = () => {
       {/* 서버에서 들어온 걸 일단 저장해두고 뿌리는 로컬 값 */}
       <div className={classes.wrapper}>
         {musicList.map((music) => (
-          <Link to={`/music/${music.musicId}`}>
+          <Link key={"link" + music.musicId} to={`/music/${music.musicId}`}>
             <div className={classes.card} key={music.musicId}>
               <img
+                key={"img" + music.musicId}
                 className={classes.albumCover}
                 src={music.image}
                 alt={`제목: ${music.title} / 가수: ${music.artist}`}
               />
-              <span className={classes.icons}>
+              <span key={"span" + music.musicId} className={classes.icons}>
                 <FontAwesomeIcon
+                  key={"icon1"}
                   icon={faBackward}
                   style={{ gridArea: "icon1" }}
                 />
-                <FontAwesomeIcon icon={faPlay} style={{ gridArea: "icon2" }} />
-                <FontAwesomeIcon icon={faPause} style={{ gridArea: "icon3" }} />
                 <FontAwesomeIcon
+                  key={"icon2"}
+                  icon={faPlay}
+                  style={{ gridArea: "icon2" }}
+                />
+                <FontAwesomeIcon
+                  key={"icon3"}
+                  icon={faPause}
+                  style={{ gridArea: "icon3" }}
+                />
+                <FontAwesomeIcon
+                  key={"icon4"}
                   icon={faForward}
                   style={{ gridArea: "icon4" }}
                 />
               </span>
-              <h3 className={classes.musicInfo}>
+              <h3 key={"h3" + music.musicId} className={classes.musicInfo}>
                 {music.title}
                 <br />
                 {music.artist}
               </h3>
-              <p>{music.contents}</p>
+              <p key={"p" + music.musicId} className={classes.musicContents}>
+                {music.contents}
+              </p>
             </div>
           </Link>
         ))}
