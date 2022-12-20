af864f68e7b88eba250f29278f26fd05ccde9e20
newrkem <newrkem@gmail.com>
2022-12-20 18:09:56 +0900

    feat: 기능구현 작업중

--- src/pages/main/home/HomeCardList.jsx
+++ src/pages/main/home/HomeCardList.jsx
@@ -1,40 +1,42 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { __getMusic } from "../../../redux/modules/todoSlice";
-import classes from "../home/HomeCardList.css";
+// import React, { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { __getMusic } from "../../../redux/modules/todoSlice";
+// import classes from "../home/HomeCardList.css";
 
-const HomeCardList = () => {
-  const dispatch = useDispatch();
-  const { isLoading, error, music } = useSelector((state) => state.music);
+// const HomeCardList = () => {
+//   //   const dispatch = useDispatch();
+//   //   const { isLoading, error, music } = useSelector((state) => state.music);
 
-  useEffect(() => {
-    dispatch(__getMusic());
-  }, [dispatch]);
+//   //   useEffect(() => {
+//   //     dispatch(__getMusic());
+//   //   }, [dispatch]);
 
-  if (isLoading) {
-    return <div>로딩중...</div>;
-  }
-  if (error) {
-    return <div>{error.message}</div>;
-  }
-  return (
-    <div className={classes.box}>
-      {music.map((music) => (
-        <div className={classes.body} key={music.musicId}>
-          {music.title}-{music.artist}
-          <img
-            src={music.image}
-            style={{
-              marginTop: "20px",
-              width: "250px",
-              height: "250px",
-            }}
-          />
-          {music.contents}
-        </div>
-      ))}
-    </div>
-  );
-};
+//   //   if (isLoading) {
+//   //     return <div>로딩중...</div>;
+//   //   }
+//   //   if (error) {
+//   //     return <div>{error.message}</div>;
+//   //   }
+//   return (
+//     <div className={classes.body}>sasdasd</div>
 
-export default HomeCardList;
+//     // <div className={classes.box}>
+//     //   {music.map((music) => (
+//     //     <div className={classes.body} key={music.musicId}>
+//     //       {music.title}-{music.artist}
+//     //       <img
+//     //         src={music.image}
+//     //         style={{
+//     //           marginTop: "20px",
+//     //           width: "250px",
+//     //           height: "250px",
+//     //         }}
+//     //       />
+//     //       {music.contents}
+//     //     </div>
+//     //   ))}
+//     // </div>
+//   );
+// };
+
+// export default HomeCardList;
