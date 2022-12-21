9c0d5ad9362a31712f7e14c25fa57b5c4977b702
newrkem <newrkem@gmail.com>
2022-12-21 16:20:04 +0900

    feat: 추가하기 기능 test2

--- src/pages/main/Add/AddList.jsx
+++ src/pages/main/Add/AddList.jsx
@@ -1,5 +1,4 @@
-import axios from "axios";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { __addMusic } from "../../../redux/modules/todoSlice";
@@ -8,88 +7,77 @@ import classes from "./AddList.module.css";
 const AddList = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [title, setTitle] = useState("");
-  const [artist, setArtist] = useState("");
-  const [content, setContent] = useState("");
-
-  const [music, setMusic] = useState({
-    title: "",
-    artist: "",
-    content: "",
+  const [data, setData] = useState({
+    data_title: "",
+    data_artist: "",
+    data_image: "",
+    data_contents: "",
   });
 
-  const [imageurl, setImageUrl] = useState(null);
-
-  const musicHandler = (e) => {
-    setTitle(e.target.value);
-    setArtist(e.target.value);
-    setContent(e.target.value);
-    setMusic(e.target.value);
+  const changeInput = (e) => {
+    const { name, value } = e.target;
+    setData({ ...data, [name]: value });
   };
 
-  const onSubmit = (e) => {
-    musicHandler(e);
+  const onAddHandler = async (e) => {
     e.preventDefault();
-
-    //   const formData = new formData();
-    //   formData.append("title", music.title);
-    //   formData.append("artist", music.artist);
-    //   formData.append("content", music.content);
-
-    //   dispatch(__addMusic(formData));
-    //   for (var pair of formData.entries()) {
-    //     console.log(pair[0] + "," + pair[1]);
-    //   }
-    //   if (!title || !artist || !content) {
-    //     return alert("빈칸 없이 입력해 주세요");
-    //   }
+    dispatch(__addMusic({ ...data }));
+    // if (!data_title || !data_artist || !data_contents) {
+    //   return alert("빈칸을 채워 주세요");
+    // }
   };
 
   return (
-    <div className={classes.header}>
-      <form>
-        <div method="post" encType="multipart/form-data">
-          <div className={classes.title}>
-            가수명:{" "}
-            <input
-              type="text"
-              name="artist"
-              size="midium"
-              placeholder="가수이름을 입력하세요"
-              onChange={musicHandler}
-            />
-            노래 제목:{""}
-            <input
-              type="text"
-              name="title"
-              size="large"
-              placeholder="제목을 입력해주세요"
-              onChange={musicHandler}
-            />
-          </div>
-          <div className={classes.image}>
-            <input type="url" name="image" onChange={musicHandler} />
-          </div>
-          <div className={classes.comment}>
-            추천 내용
-            <input
-              size="textArea"
-              name="content"
-              placeholder="내용을 입력해주세요"
-              onChange={musicHandler}
-            />
-          </div>
-          <button
-            className={classes.btn}
-            type="submit"
-            form="add"
-            onClick={onSubmit}
-          >
-            등록
-          </button>
-        </div>
-      </form>
-    </div>
+    <form method="post" className={classes.box}>
+      <h2 className={classes.head}>등록하기</h2>
+      <div className={classes.title}>
+        <input
+          type="text"
+          name="data_artist"
+          value={data.data_artist}
+          maxLength="20"
+          placeholder="가수이름을 입력하세요"
+          onChange={changeInput}
+        />
+        <input
+          type="text"
+          name="data_title"
+          value={data.data_title}
+          maxLength="20"
+          placeholder="제목을 입력해주세요"
+          onChange={changeInput}
+        />
+      </div>
+      <input
+        style={{ width: "500px", height: "30px" }}
+        type="text"
+        name="data_image"
+        placeholder="이미지 URL"
+        onChange={changeInput}
+      />
+      <div className={classes.comment}>
+        <textarea
+          style={{ width: "500px" }}
+          rows="4"
+          cols="50"
+          required
+          name="data_contents"
+          value={data.data_contents}
+          placeholder="내용을 입력해주세요"
+          onChange={changeInput}
+        />
+      </div>{" "}
+      <br />
+      <footer className={classes.btn}>
+        <button style={{ width: "100px" }} onSubmit={onAddHandler}>
+          등록
+        </button>
+        &nbsp;
+        <button style={{ width: "100px" }} onClick={() => navigate("/")}>
+          뒤로가기
+        </button>
+      </footer>
+    </form>
   );
 };
 
