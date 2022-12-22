e61a1cb22586d125c27fd0c2b84e88d7c8875c78
Ryan Kwon <dev.ryan.kwon@gmail.com>
2022-12-22 14:26:18 +0900

    test: 카카오 로그인

--- src/pages/Login/KakaoLogin.js
+++ src/pages/Login/KakaoLogin.js
@@ -7,7 +7,6 @@ import {
   useNavigate,
 } from "react-router-dom"
 import { useEffect } from "react"
-import { REDIRECT_URI, REST_API_KEY } from "./kakaoApi"
 import axios from "axios"
 import { apis, DB } from "../../shared/api"
 
@@ -16,7 +15,7 @@ const KakaoLogin = () => {
   const navigate = useNavigate()
   const KAKAO_CODE = location.search.split("=")[1]
 
-  const getKakaoToken = () => {
+  /*   const getKakaoToken = () => {
     fetch(`https://kauth.kakao.com/oauth/token`, {
       method: "POST",
       headers: {
@@ -32,12 +31,12 @@ const KakaoLogin = () => {
           navigate("/")
         }
       })
-  }
+  } */
   useEffect(() => {
     if (!location.search) return
     // apis.kakao(KAKAO_CODE)
     // axios.get(`https://kauth.kakao.com/oauth/token`)
-    getKakaoToken()
+    // getKakaoToken()
     axios
       // .post(`http://192.168.5.7:8080/api/login/kakao?code=${KAKAO_CODE}`)
       .get(`${DB}/api/login/kakao?code=${KAKAO_CODE}`)
